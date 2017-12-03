d45257eb42b183a737149b0a46a264abbd004c0e
Steven Blowers <shdblowers@gmail.com>
2017-12-03 22:51:14 +0000

    not working but finding a way

--- src/main/scala/InverseCaptcha.scala
+++ src/main/scala/InverseCaptcha.scala
@@ -1,5 +1,18 @@
-class InverseCaptcha(val input: String) {
+class InverseCaptcha(val input: Array[Int]) {
   def solve(): Int = {
     3
+  }
+}
+
+object InverseCaptcha {
+  def apply(input: String): InverseCaptcha = {
+    val splitInput: Array[Int] =
+      input
+        .split("")
+        .map((numberAsString: String) => {
+          numberAsString.toInt
+        })
+
+    new InverseCaptcha(splitInput)
   }
 }
--- src/test/scala/InverseCaptchaTest.scala
+++ src/test/scala/InverseCaptchaTest.scala
@@ -1,8 +1,14 @@
 import org.scalatest.FunSuite
 
 class InverseCaptchaTest extends FunSuite {
-  test("InverseCaptcha.solve") {
-    val simpleExample = new InverseCaptcha("1122")
+  test("InverseCaptcha.solve simple") {
+    val simpleExample: InverseCaptcha = InverseCaptcha("1122")
     assert(simpleExample.solve() === 3)
+  }
+
+  test("InverseCaptcha.solve none the same") {
+    val willBeZero: InverseCaptcha = InverseCaptcha("123456789")
+
+    assert(willBeZero.solve() === 0)
   }
 }
